5576192a2ad0d0567718cbe017f602bbbdf4032c
Klaudio <klaudiomaraj@outlook.it>
2022-05-06 12:07:28 +0200

    admin order deliver

--- server/Routes/orderRoutes.js
+++ server/Routes/orderRoutes.js
@@ -109,4 +109,24 @@ orderRouter.put(
   })
 );
 
+// ORDER IS DELIVERED
+orderRouter.put(
+  "/:id/delivered",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+      
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
+    }
+  })
+);
+
 export default orderRouter;
